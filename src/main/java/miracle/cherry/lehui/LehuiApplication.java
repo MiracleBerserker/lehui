3973db37232b5b3211537bfd6a5a534a7eee2c99
miracle <873144200@qq.com>
2019-07-14 12:40:25 +0800

    Initial commit

---
+++ src/main/java/miracle/cherry/lehui/LehuiApplication.java
@@ -0,0 +1,13 @@
+package miracle.cherry.lehui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LehuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LehuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miracle/cherry/lehui/LehuiApplicationTests.java
@@ -0,0 +1,16 @@
+package miracle.cherry.lehui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LehuiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
